678f7a1813633506b2e6c29d451f71709272e1be
Ryan T. Hamilton <astrobokonon@gmail.com>
2019-11-12 15:19:29 -0700

    Starting to flesh out listener/router

--- EatAtJoes/superSnooper.py
+++ EatAtJoes/superSnooper.py
@@ -17,7 +17,65 @@ from __future__ import division, print_function, absolute_import
 
 import time
 
+import stomp
+import xmltodict as xmld
+
 from ligmos import utils
+
+
+class joeListener(stomp.listener.ConnectionListener):
+    """
+    """
+    def __init__(self, dictify=True):
+        self.dictify = dictify
+
+    # Subclassing stomp.listener.ConnectionListener
+    def on_message(self, headers, body):
+        tname = headers['destination'].split('/')[-1]
+        # Manually turn the bytestring into a string
+        try:
+            body = body.decode("utf-8")
+            badMsg = False
+        except Exception as err:
+            print(str(err))
+            print("Badness 10000")
+            print(body)
+            badMsg = True
+
+        if badMsg is False:
+            try:
+                if self.dictify is True:
+                    xml = xmld.parse(body)
+                    res = {tname: [headers, xml]}
+                else:
+                    # If we want to have the XML as a string:
+                    res = {tname: [headers, body]}
+
+            except xmld.expat.ExpatError:
+                # This means that XML wasn't found, so it's just a string
+                #   packet with little/no structure. Attach the sub name
+                #   as a tag so someone else can deal with the thing
+                res = {tname: [headers, body]}
+            except Exception as err:
+                # This means that there was some kind of transport error
+                #   or it couldn't figure out the encoding for some reason.
+                #   Scream into the log but keep moving
+                print("="*42)
+                print(headers)
+                print(body)
+                print("="*42)
+                badMsg = True
+
+        print("Message Source: %s" % (tname))
+        print("Message Headers:", headers)
+        if badMsg:
+            print("Header: %s" % (headers))
+            print("Body: %s" % (body))
+        else:
+            if self.dictify is True:
+                print(res)
+            else:
+                print(body)
 
 
 def main(brokerHost, topics, brokerPort=61613):
@@ -26,7 +84,7 @@ def main(brokerHost, topics, brokerPort=61613):
     bigsleep = 30
 
     print("Setting up listener...")
-    listener = utils.amq.ParrotSubscriber(dictify=False)
+    listener = joeListener(dictify=False)
 
     conn = utils.amq.amqHelper(brokerHost,
                                topics,
